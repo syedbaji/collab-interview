442811a202fc7f8bed3b7cdae89bf781a8920c32
Syed Baji Shaik <syedbs@langoor.com>
2021-04-30 16:41:34 +0530

    fix

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    fillForm('display');
+
     $('#accordion .card .collapse').each(function(){
         if($(this).hasClass('show')){
             $(this).closest('.card').addClass('active-card');
@@ -11,4 +13,32 @@ $(document).ready(function(){
     $(".collapse").on('hide.bs.collapse', function(){
         $(this).closest('.card').removeClass('active-card');
     });
+    $('.edit-info').on('click', function(){
+        $(this).toggleClass('d-none');
+        $('.cancel-edit, .update-edited, .reset-edit').toggleClass('d-none');
+        $('form').removeClass('in-display-mode').addClass('in-edit-mode');
+        fillForm('edit');
+    });
+    $('.cancel-edit, .update-edited').on('click', function(){
+        $('.cancel-edit, .update-edited, .reset-edit').toggleClass('d-none');
+        $('.edit-info').toggleClass('d-none');
+        $('form').removeClass('in-edit-mode').addClass('in-display-mode');
+        fillForm('display');
+    });
+    $('input[name="workingNoworkingRadio"]').on('change', function(){
+        $('.only-if-working').removeClass('working notworking').addClass($('input[name="workingNoworkingRadio"]:checked').val())
+    });
+    function fillForm(mode){
+        $('form input').each(function(){
+            if(mode === 'edit'){
+                if($(this).val().length < 1 || $(this).val() === "-" || $(this).val() === " " || $(this).val() === ""){
+                    $(this).val('');
+                }
+            }else{
+                if($(this).val().length < 1){
+                    $(this).val('-');
+                }
+            }
+        });
+    }
 });
